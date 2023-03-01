b479d9965fca985bc69a3f5cbd738697cb2fca8a
marat-a <detishki66@yandex.ru>
2023-03-01 09:53:58 +0500

    solved

--- src/DiegoCollection.java
+++ src/DiegoCollection.java
@@ -1,41 +1,41 @@
-import com.sun.source.tree.Tree;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 public class DiegoCollection {
     public static void main(String[] args) throws Exception {
-        final LocalDateTime start = LocalDateTime.now();
-        final BufferedReader r = new BufferedReader(new FileReader("input14.txt"));
+        final BufferedReader r = new BufferedReader(new FileReader("input.txt"));
         final int quantity = Integer.parseInt(r.readLine());
         final String[] numbersOfStickers = r.readLine().split(" ");
-        ArrayList<Integer> numbersOfDiegoStickers = new ArrayList<>(quantity);
-        for (int i = 0; i < numbersOfStickers.length; i++) {
-            numbersOfDiegoStickers.add(Integer.parseInt(numbersOfStickers[i]));
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(-1);
+        for (String numbersOfSticker : numbersOfStickers) {
+            treeSet.add(Integer.parseInt(numbersOfSticker));
         }
+        ArrayList<Integer> numbersOfDiegoStickers = new ArrayList<>(quantity);
+        numbersOfDiegoStickers.addAll(treeSet);
         final int quantityCollectors = Integer.parseInt(r.readLine());
         final String[] minNumberNeeded = r.readLine().split(" ");
         int minNumber;
         for (int i = 0; i < quantityCollectors; i++) {
             minNumber = Integer.parseInt(minNumberNeeded[i]);
             int index = calcInterestingCards(minNumber, numbersOfDiegoStickers);
-            System.out.println(numbersOfDiegoStickers.size()-index);
+            System.out.println(index);
         }
-        System.out.println(Duration.between(start, LocalDateTime.now()).toMillis());
     }
 
-    static int calcInterestingCards(int minNumber, ArrayList<Integer> diegoStickers){
+    static int calcInterestingCards(int minNumber, ArrayList<Integer> diegoStickers) {
         int left = 0;
-        int right = diegoStickers.size()-1;
-        int index = 0;
-        while (left<right) {
-            int mid = (left  + right +1) / 2;
-            if ()
+        int right = diegoStickers.size() - 1;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (diegoStickers.get(mid) < minNumber) {
+                left = mid;
+            } else right = mid - 1;
 
         }
-        return index;
+        return left;
     }
 }
